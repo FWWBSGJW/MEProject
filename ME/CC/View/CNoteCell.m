04e56270107da44d0a974ea63a24c072213f94d9
YatoKami <chenpingguo@chenpingguodeMacBook-Pro.local>
2014-08-03 22:09:25 +0800

    同步

--- ME/CC/View/CNoteCell.m
+++ ME/CC/View/CNoteCell.m
@@ -4,7 +4,7 @@
 //
 //  Created by yato_kami on 14-8-1.
 //  Copyright (c) 2014年 yatokami. All rights reserved.
-//
+//同步
 
 #import "CNoteCell.h"
 
@@ -12,7 +12,7 @@
 
 - (void)awakeFromNib
 {
-    // Initialization code
+    
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
